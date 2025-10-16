43b931d93695ee0faa8ccc75ac1f33753766cc2d
Eric Savage <eric.n.savage2000@gmail.com>
2025-10-16 09:00:32 -0400

    plotted dendogram for iris dataset installed pyqt5

--- iris_hierarchical_test.py
+++ iris_hierarchical_test.py
@@ -3,3 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from sklearn.datasets import load_iris
+
+
+# Load the Iris dataset
+iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv').values[:, :-1]
+
+# choose linkage array for clustering performance
+linkage_array = linkage(iris, method='average', metric="euclidean")
+
+# Plot the dendrogram
+dendrogram(linkage_array)
+plt.show()
+
+# extract clusters from linkage array for a certain threshhold and then assign clusters to the data points
+cluster_assignments = fcluster(linkage_array, t=1.5, criterion='distance')
+print(cluster_assignments)
