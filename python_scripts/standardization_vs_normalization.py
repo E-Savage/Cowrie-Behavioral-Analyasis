257103480597cb3d31496b438d3f7b34f394c456
Eric Savage <esavage1@umassd.edu>
2026-03-31 13:55:27 +0000

    changed some of the directories moved files around added folders slurm scripts and python scripts

---
+++ python_scripts/standardization_vs_normalization.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.cluster import KMeans
+from sklean.metrics import silhouette_score
